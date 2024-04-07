247181e8ff860005b46d969de2b3dbb79092e8f5
DanielMctavish <dc.uneb@gmail.com>
2024-04-07 09:29:05 -0300

    backend-aukt-update

--- src/http/routes/AdvertiserRoutes.ts
+++ src/http/routes/AdvertiserRoutes.ts
@@ -10,25 +10,29 @@ const upload = multer()
 const mainAdvertiser = new MainAdvertiserUsecases()
 
 router.post('/create-advertiser', ApplyUseCase(mainAdvertiser.CreateAdvertiser))//testado x2
-router.get('/find',ApplyUseCase(mainAdvertiser.FindAdvertiser))//testado
+router.get('/find', ApplyUseCase(mainAdvertiser.FindAdvertiser))//testado
 router.get('/find-by-email', verifyToken, ApplyUseCase(mainAdvertiser.FindAdvertiserByEmail))//testado
 router.patch('/update', verifyToken, ApplyUseCase(mainAdvertiser.UpdateAdvertiser))//testado
 router.delete('/delete', verifyToken, ApplyUseCase(mainAdvertiser.DeleteAdvertiser))//testado
 
-router.get('/verify',(req,res)=>{
+router.get('/verify', (req, res) => {
     res.send('ok! rota acessada')
 })
 
 router.post("/login", ApplyUseCase(mainAdvertiser.LoginAdvertiser))//testado
 
 //data/params/file/files
-router.post("/upload-cover-profile", verifyToken, upload.single('cover-blog-post'), ApplyUseCase(mainAdvertiser.FirebaseUploadPhotoProfile))
+router.post("/upload-cover-profile", verifyToken,
+    upload.single('cover-blog-post'), ApplyUseCase(mainAdvertiser.FirebaseUploadPhotoProfile))
 
-router.delete("/delete-profile", verifyToken, ApplyUseCase(mainAdvertiser.FirebaseDeletePhotoProfile))
+router.delete("/delete-profile", verifyToken,
+    ApplyUseCase(mainAdvertiser.FirebaseDeletePhotoProfile))
 
-router.post("/upload-logo-company", verifyToken, upload.single('cover-blog-post'), ApplyUseCase(mainAdvertiser.FirebaseUploadLogoCompany))
+router.post("/upload-logo-company", verifyToken,
+    upload.single('cover-blog-post'), ApplyUseCase(mainAdvertiser.FirebaseUploadLogoCompany))
 
-router.delete("/delete-logo-company", verifyToken, ApplyUseCase(mainAdvertiser.FirebaseDeleteLogoCompany))
+router.delete("/delete-logo-company", verifyToken,
+    ApplyUseCase(mainAdvertiser.FirebaseDeleteLogoCompany))
 
 
 export default router;
